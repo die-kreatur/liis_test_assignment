8ffd264cbb1d99decf12e1283c9f17575cf3adae
die-kreatur <parusmajor@list.ru>
2022-01-08 20:41:22 +0300

    Create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    """Place model"""
+    number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"Place {self.number}"
+
+
+class Booking(models.Model):
+    """Model for booking places"""
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    date_from = models.DateField()
+    date_to = models.DateField()
+
+    def __str__(self):
+        return f"Booking of {self.place.number} {self.date_from} - {self.date_to}"
